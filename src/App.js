3d2c5e7d32a3b6e39bd68eb5d354337b6445f616
angelo.p <mrpalaiologos@gmail.com>
2021-06-24 16:06:51 +0200

    checking out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>im angelos </div>;
+  return (
+    <div>
+      im angelos
+      {/* object-fit :contain ; */}
+      <p>
+        npm i firebase https://www.npmjs.com/package/react-tinder-card 1.36
+        firebase
+      </p>
+    </div>
+  );
 }
 
 export default App;
