eaa6dbe00903a9bdbcf7fc5b6a7fbdff890aae56
hamza <darejhamza@gmail.com>
2021-06-24 15:31:53 +0200

    all needed icons

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './sass/main.scss';
 import App from './App';
 
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
