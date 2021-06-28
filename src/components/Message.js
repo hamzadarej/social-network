73ad882b3d9bc088ae310b0f9d45280381bd6bcc
angelo.p <mrpalaiologos@gmail.com>
2021-06-28 11:41:10 +0200

    start working on messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,19 +2,17 @@ import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import { Link } from "react-router-dom";
 //the avatar gives them the form of profile pics ,if they get a pic link shows the pic if not shows the icon
-function Message(props, message, timestamp, pic) {
-  console.log(props.name);
-
+function Message(props) {
   return (
     <Link to={`/messages/${props.name}`}>
       <div className="message">
-        <Avatar className="chat_image" alt={props.name} src={props.pic} />
+        <Avatar className="message_image" alt={props.name} src={props.pic} />
         <div className="message-details">
           {/* use .message_details {flex:1;} in css ;) */}
           <h2>{props.name}</h2>
           <p>{props.message}</p>
         </div>
-        <p className="time">{props.timestamp}</p>
+        <p className="timestamp">{props.timestamp}</p>
       </div>
     </Link>
   );
