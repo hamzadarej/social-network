a4cd36c2734df824cbffc28d3845cd6fffb039f7
hamza <darejhamza@gmail.com>
2021-06-29 17:02:14 +0200

    change

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,52 +1,28 @@
-import React, { useState ,useEffect} from "react";
-import Data from "../Data.json";
-
-import TinderCard from "react-tinder-card";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-//for the pressing button effect, wrap your  FaChevronLeft, FaChevronRight, with that ;)
-import { IconButton } from "@material-ui/core/";
-{
-  /*  use for the pics object-fit :contain, it will save u; */
-}
-
-
-
-
+import React ,{useState} from "react";
+import Data from "../Data.json"
 function Cards() {
   //TINDER CARDS SWIPING PART
-  const onSwipe = (direction) => { 
-    console.log(direction)
+  const onSwipe = (direction) => {
+    console.log(direction);
     console.log("You swiped: " + direction);
   };
-  
-   
+
   const onCardLeftScreen = (myIdentifier) => {
     console.log(myIdentifier + " left the screen");
   };
   const [dataArr, setDataArr] = useState(Data);
-  console.log(dataArr)
-
-  
+  console.log(dataArr);
 
   //TINDER CARDS SWIPING PART
 
- 
-        
-       
-
-    
-    
-
-  var cartData =dataArr.map((obj) => {
-    const {img,name,id}=obj;
+  var cartData = dataArr.map((obj) => {
+    const { img, name, id } = obj;
     console.log(dataArr);
     const stylepic = `url(${Data[1].img}/${Math.floor(Math.random() * 1000)})`;
-let deleteProfil=(id, arr)=> {
-    
-setDataArr(arr.filter((item) => item.id !==id));
-    
-  };
 
+    let deleteProfil = (id, arr) => {
+      setDataArr(arr.filter((item) => item.id !== id));
+    };
 
     return (
       <div className="card-wrapper">
@@ -54,23 +30,18 @@ setDataArr(arr.filter((item) => item.id !==id));
           <FaChevronLeft />
         </IconButton> */}
 
-
         {/* TINDER CARDS SWIPING PART */}
         <div
           className="swipe"
           onSwipe={onSwipe}
           onCardLeftScreen={() => onCardLeftScreen("fooBar")}
-
           preventSwipe={["right", "left"]}
           preventSwipe={["up", "down"]}
-
-          
         >
-          <button 
+          <button
             onClick={() => {
-              deleteProfil(id,dataArr)  ;
-              
-            }} 
+              deleteProfil(id, dataArr);
+            }}
           >
             delete
           </button>
@@ -90,14 +61,12 @@ setDataArr(arr.filter((item) => item.id !==id));
         {/* TINDER CARDS SWIPING PART */}
 
         <IconButton>
-          <FaChevronRight onClick={()=>onSwipe("right")} />
+          <FaChevronRight onClick={() => onSwipe("right")} />
         </IconButton>
-
 
         {/* <IconButton>
           <FaChevronRight />
         </IconButton> */}
-
       </div>
     );
   });
